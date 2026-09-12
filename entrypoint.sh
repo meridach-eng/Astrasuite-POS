316687839cc037207b7b919a5109bd7a99e9c9c5
meridach <meridach@K17W6H4Y-4266A3.local>
2026-09-12 00:24:10 -0600

    Regenerar autoloader en el entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+composer dump-autoload --optimize --no-dev --no-interaction
 php artisan optimize:clear
 php artisan config:clear
 php artisan cache:clear
-php artisan route:cache
+php artisan route:clear
 php artisan storage:link --force
 exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
