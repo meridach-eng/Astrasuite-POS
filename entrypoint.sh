80d1ca8edf4a0897fa339e02325a81b2be0d1351
meridach <meridach@K17W6H4Y-4266A3.local>
2026-09-12 00:47:00 -0600

    Cambios del despliegue

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-composer dump-autoload --optimize --no-dev --no-interaction
-php artisan optimize:clear
-php artisan config:clear
-php artisan cache:clear
-php artisan route:clear
-php artisan storage:link --force
-exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
